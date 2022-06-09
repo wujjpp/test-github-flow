0702556aeab5bd99b9466d3804f8b9849b174c4c
Wu Jian Ping <wujp@greatld.com>
2022-06-09 13:55:04 +0800

    .

--- .nebula-up/check.sh
+++ .nebula-up/check.sh
@@ -3,17 +3,19 @@
 retries=0
 while :
 do
-  retries=$((retries+1))
-
   if curl -sL --fail http://127.0.0.1:19669;
     then break
   fi
+
+  docker-compose ps
 
   if [ $retries -gt 120 ];
     then break
   fi
 
-  sleep 1
+  sleep 2
 
-  echo "retries=${retries}"
+  retries=$((retries+1))
+
+  echo "check: ${retries}"
 done
